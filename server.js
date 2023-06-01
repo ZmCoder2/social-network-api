d6804a4e019c5937f32eed9c56afa9036d2d6d88
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-05-31 21:30:36 -0400

    npm packages installed

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// Current working directory
+const cwd = process.cwd();
+
+
+// https:localhost:3001
+const PORT = 3001;
+const app = express();
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log//(`API server for ${} running on port ${PORT}!`);
+    });
+});
