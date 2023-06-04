d23db21ca241200baf8b958fab22a4eb0968cd84
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-04 18:34:27 -0400

    thoughtRoutes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const {
-    
+    getAllThoughts,
+    getSingleThoughtById,
+    createNewThought,
+    updateThoughtById,
+    deleteThoughtById,
+    createReaction,
+    deleteReaction
 } = require('../../controllers/thoughtsController');
+
+router.route('/').get(getAllThoughts).post(createNewThought);
+router.route('/:id').get(getSingleThoughtById).put(updateThoughtById).delete(deleteThoughtById);
+router.route('/:id/reactions').post(createReaction);
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
