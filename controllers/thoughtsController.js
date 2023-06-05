b5cd0543d8c64aa761977fa2742453d7a5b892df
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-04 20:53:01 -0400

    updateThoughtById

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -17,7 +17,7 @@ module.exports = {
     // Get a single thought by its ID
     async getSingleThoughtById(req, res) {
         try {
-            const thought = await Thoughts.findOne({ _id: req.params.postId })
+            const thought = await Thoughts.findOne({ _id: req.params.thoughtsId })
             .populate({ path: 'reactions', select: '-__v'});
 
             if (thought) {
@@ -56,13 +56,47 @@ module.exports = {
     // Create a Thought
     async createNewThought(req, res) {
         try {
-            const thought = await Thoughts.create(req.body);
-            res.json(thought);
+            const thoughts = await Thoughts.create(req.body);
+            res.json(thoughts);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
         }
-    }
+    },
+
+    // Delete a Thought by ID
+    async deleteThoughtById(req, res) {
+        try {
+            const thoughts = await Thoughts.findOneAndDelete({ _id: req.params.thoughtsId });
+
+            if (!thoughts) {
+                res.status(404).json({ message: 'No thoughts with that ID'});
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Update a thought
+    async updateThoughtById(req, res) {
+        try {
+            const thoughts = await Thoughts.findOneAndUpdate(
+                { _id: req.params.thoughtsId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thoughts) {
+                res.status(404).json({ message: 'No thoughts with that ID' });
+            }
+
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    
 };
 
 // module.exports = thoughtsController;
